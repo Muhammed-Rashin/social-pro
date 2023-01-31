900183dedaf538e676422aa581eeff446c3715ab
Muhammed-Rashin <ceorashin@gmail.com>
2023-02-01 01:55:50 +0530

    second commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:5000',
-  baseURL: 'https://backend.pradax.online',
+  baseURL: 'http://localhost:5000',
+  // baseURL: 'https://backend.pradax.online',
   withCredentials: true,
   headers: {
     token: localStorage.getItem('Accesstoken'),
